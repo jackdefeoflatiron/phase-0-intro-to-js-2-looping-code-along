b0e225fb50c23bc657d3dce42493f370ead8b886
John DeFeo <jackd3243@gmail.com>
2021-11-04 13:37:49 -0400

    commit tree 4d02cc9f3be96abfe02337f1663201c879c63a3a

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solutions in this file
+const messages = [];
+
+function writeCards(arrayOfNames, event){
+    for(let i =0; i < arrayOfNames.length; i++) {
+        messages.push(`Thank you, ${arrayOfNames[i]}, for the wonderful ${event} gift!`)
+    }
+        return messages;
+}
+
+function countDown(numbers){
+    while(numbers >= 0) {
+            console.log(numbers--)}
+    
+}
